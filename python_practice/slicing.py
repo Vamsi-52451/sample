f1b51dbbf904695b1094c62fa4f4a106b1f2ed70
somandham vamsi <somanadhamvamsi@gmail.com>
2025-08-25 12:50:34 +0530

    Create slicing.py

---
+++ python_practice/slicing.py
@@ -0,0 +1,21 @@
+text = "Hello, World!"
+
+print("Original string:", text)
+print("text[0:5]:", text[0:5])       
+print("text[7:]:", text[7:])         
+print("text[:5]:", text[:5])         
+print("text[-6:-1]:", text[-6:-1])   
+print("text[::2]:", text[::2])       
+print("text[::-1]:", text[::-1])     
+
+print("\n")
+
+# List slicing examples
+numbers = [0, 1, 2, 3, 4, 5, 6]
+
+print("Original list:", numbers)
+print("numbers[2:5]:", numbers[2:5])  
+print("numbers[:3]:", numbers[:3])     
+print("numbers[3:]:", numbers[3:])     
+print("numbers[::2]:", numbers[::2])   
+print("numbers[::-1]:", numbers[::-1]) 
