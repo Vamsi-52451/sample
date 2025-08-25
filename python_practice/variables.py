64accfc1264121e151a0f414da9eead9e4f4f411
somandham vamsi <somanadhamvamsi@gmail.com>
2025-08-25 13:05:15 +0530

    Create variables.py

---
+++ python_practice/variables.py
@@ -0,0 +1,20 @@
+# Assign values to variables
+x = 10               
+name = "Alice"       
+pi = 3.14           
+is_valid = True      
+
+# Print variable values
+print("x =", x)
+print("name =", name)
+print("pi =", pi)
+print("is_valid =", is_valid)
+
+# Variables can be reassigned
+x = 20
+print("New value of x =", x)
+
+# Multiple assignment
+a, b, c = 1, 2, 3
+print("a =", a, "b =", b, "c =", c)
+
