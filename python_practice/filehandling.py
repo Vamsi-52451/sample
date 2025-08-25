7c559bc7299efd29f54462c378bf69b591912bd3
somandham vamsi <somanadhamvamsi@gmail.com>
2025-08-25 13:09:37 +0530

    Create filehandling.py

---
+++ python_practice/filehandling.py
@@ -0,0 +1,13 @@
+# Writing to a file
+with open("example.txt", "w") as file:
+    file.write("Hello, world!\n")
+    file.write("This is a sample file.\n")
+
+print("Data written to file.")
+
+# Reading from a file
+with open("example.txt", "r") as file:
+    content = file.read()
+
+print("File content:")
+print(content)
