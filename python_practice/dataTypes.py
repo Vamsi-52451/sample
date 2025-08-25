3b4ee65530a2bdabfda77e82b0a5bbccae54fde2
somandham vamsi <somanadhamvamsi@gmail.com>
2025-08-25 12:25:07 +0530

    dataTypes

---
+++ python_practice/dataTypes.py
@@ -0,0 +1,32 @@
+# Integer
+x = 10
+print(x, type(x))
+
+# Float
+y = 3.14
+print( y, type(y))
+
+# String
+name = "Alice"
+print( name, type(name))
+
+# Boolean
+is_valid = True
+print(is_valid, type(is_valid))
+
+# List
+fruits = ["apple", "banana", "cherry"]
+print(fruits, type(fruits))
+
+# Tuple
+coordinates = (10, 20)
+print(coordinates, type(coordinates))
+
+# Set
+unique_numbers = {1, 2, 3, 2}
+print(unique_numbers, type(unique_numbers))
+
+# Dictionary
+person = {"name": "Bob", "age": 25}
+print("Dictionary:", person, type(person))
+
